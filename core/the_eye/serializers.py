4505a27a7cd09f729046a77b3a408fb76b25217b
FeliPython <felipe.morais@nubank.com.br>
2021-03-06 11:30:15 -0300

    Setup the serializer for event using ModelSerializer

---
+++ core/the_eye/serializers.py
@@ -0,0 +1,8 @@
+from .models import Event
+from rest_framework import serializers
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['session_id', 'category', 'name', 'data', 'timestamp']
