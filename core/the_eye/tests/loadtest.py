44c50e50a42737c124c24c8b38ed3d444044f17b
FeliPython <felipe.morais@nubank.com.br>
2021-03-07 00:10:31 -0300

    apply black

--- core/the_eye/tests/loadtest.py
+++ core/the_eye/tests/loadtest.py
@@ -6,14 +6,17 @@ class WebsiteUser(HttpUser):
 
     @task
     def events(self):
-        self.client.post("/events/", json={
-            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
-            "category": "page interaction",
-            "name": "cta click",
-            "data": {
-                "host": "www.consumeraffairs.com",
-                "path": "/",
-                "element": "chat bubble"
+        self.client.post(
+            "/events/",
+            json={
+                "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+                "category": "page interaction",
+                "name": "cta click",
+                "data": {
+                    "host": "www.consumeraffairs.com",
+                    "path": "/",
+                    "element": "chat bubble",
+                },
+                "timestamp": "2021-01-01 09:15:27.243860",
             },
-            "timestamp": "2021-01-01 09:15:27.243860"
-        })
+        )
